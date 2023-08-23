a43c8e1dfd34406f363e0635020b0ac8af4ab54d
Sopen <58549604+sopenlaz0@users.noreply.github.com>
2023-08-23 16:05:44 +0700

    fixing search field in sale delivery

--- src/components/input/BPAutoComplete.tsx
+++ src/components/input/BPAutoComplete.tsx
@@ -1,29 +1,44 @@
-import BusinessPartnerRepository from "@/services/actions/bussinessPartnerRepository"
-import { Autocomplete, Box, CircularProgress, TextField } from "@mui/material"
-import React from "react"
-import { BsDot } from "react-icons/bs"
-import { useQuery } from "react-query"
+import BusinessPartnerRepository from "@/services/actions/bussinessPartnerRepository";
+import { Autocomplete, Box, CircularProgress, TextField } from "@mui/material";
+import React from "react";
+import { BsDot } from "react-icons/bs";
+import { useQuery } from "react-query";
 
 export default function BPAutoComplete(props: {
-  label?: any
-  type?: "Customer" | "Supplier"
+  label?: any;
+  type?: "Customer" | "Supplier";
+  value?: string;
+  onChange?: (value: string) => void
+
 }) {
-  const { type = "Customer" } = props
+  const { type = "Customer" } = props;
   const { data, isLoading }: any = useQuery({
     queryKey: [`venders_${type}`],
     queryFn: () =>
       new BusinessPartnerRepository().get(`&$filter=CardType eq 'c${type}'`),
     staleTime: Infinity,
-  })
-  const [value, setValue] = React.useState()
+  });
+  const [value, setValue] = React.useState();
   return (
     <div className="block">
-      <label htmlFor="">{props?.label || "Vendor/Customer"}</label>
+      <label
+        htmlFor=""
+        className={` text-[14px] xl:text-[13px] text-[#656565] mt-1`}
+      >
+        {props?.label || "Vendor/Customer"}
+      </label>
+
       <Autocomplete
         options={data}
         autoHighlight
-        value={value}
-        onChange={(event, newValue) => setValue(newValue)}
+        // value={value}
+        // onChange={(event, newValue) => setValue(newValue)}
+        value={props.value ? value : null}  // Use the passed value prop directly
+        onChange={(event, newValue) => {
+          if (props.onChange) {
+            props.onChange(newValue || ''); // Call the handler with newValue or an empty string
+          }
+        }}
         loading={isLoading}
         getOptionLabel={(option: any) => option.CardCode}
         renderOption={(props, option) => (
@@ -40,7 +55,9 @@ export default function BPAutoComplete(props: {
               ...params.InputProps,
               endAdornment: (
                 <React.Fragment>
-                  {isLoading ? <CircularProgress color="inherit" size={20} /> : null}
+                  {isLoading ? (
+                    <CircularProgress color="inherit" size={20} />
+                  ) : null}
                   {params.InputProps.endAdornment}
                 </React.Fragment>
               ),
@@ -49,5 +66,5 @@ export default function BPAutoComplete(props: {
         )}
       />
     </div>
-  )
+  );
 }
--- src/presentations/sale/sale_delivery/index.tsx
+++ src/presentations/sale/sale_delivery/index.tsx
@@ -1,18 +1,20 @@
-import request, { url } from "@/utilies/request"
-import React from "react"
-import { useQuery } from "react-query"
-import { useNavigate } from "react-router-dom"
-import DataTable from "./components/DataTable"
-import VisibilityIcon from "@mui/icons-material/Visibility"
-import DriveFileRenameOutlineIcon from "@mui/icons-material/DriveFileRenameOutline"
-import MUITextField from "@/components/input/MUITextField"
-import BPAutoComplete from "@/components/input/BPAutoComplete"
-import { Button } from "@mui/material"
-import { ModalAdaptFilter } from "./components/ModalAdaptFilter"
+import request, { url } from "@/utilies/request";
+import React from "react";
+import { useQuery } from "react-query";
+import { useNavigate } from "react-router-dom";
+import DataTable from "./components/DataTable";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import DriveFileRenameOutlineIcon from "@mui/icons-material/DriveFileRenameOutline";
+import MUITextField from "@/components/input/MUITextField";
+import BPAutoComplete from "@/components/input/BPAutoComplete";
+import { Button } from "@mui/material";
+import { ModalAdaptFilter } from "./components/ModalAdaptFilter";
+import { BiFilterAlt } from "react-icons/bi";
+import DataTableColumnFilter from "@/components/data_table/DataTableColumnFilter";
 
 export default function DeliveryLists() {
-  const [open, setOpen] = React.useState<boolean>(false)
-  const route = useNavigate()
+  const [open, setOpen] = React.useState<boolean>(false);
+  const route = useNavigate();
   const columns = React.useMemo(
     () => [
       {
@@ -57,10 +59,11 @@ export default function DeliveryLists() {
                 route("/sale/delivery/" + cell.row.original.DocEntry, {
                   state: cell.row.original,
                   replace: true,
-                })
+                });
               }}
             >
-              <VisibilityIcon fontSize="small" className="text-gray-600 " /> View
+              <VisibilityIcon fontSize="small" className="text-gray-600 " />{" "}
+              View
             </button>
             <button
               className="bg-transparent text-gray-700 px-[4px] py-0 border border-gray-200 rounded"
@@ -68,7 +71,7 @@ export default function DeliveryLists() {
                 route("/sale/delivery/" + cell.row.original.DocEntry, {
                   state: cell.row.original,
                   replace: true,
-                })
+                });
               }}
             >
               <DriveFileRenameOutlineIcon
@@ -82,14 +85,14 @@ export default function DeliveryLists() {
       },
     ],
     []
-  )
+  );
 
-  const [filter, setFilter] = React.useState("")
-  const [sortBy, setSortBy] = React.useState("")
+  const [filter, setFilter] = React.useState("");
+  const [sortBy, setSortBy] = React.useState("");
   const [pagination, setPagination] = React.useState({
     pageIndex: 0,
     pageSize: 10,
-  })
+  });
 
   const Count: any = useQuery({
     queryKey: ["pa-count" + filter !== "" ? "-f" : ""],
@@ -100,15 +103,18 @@ export default function DeliveryLists() {
       )
         .then(async (res: any) => res?.data)
         .catch((e: Error) => {
-          throw new Error(e.message)
-        })
-      return response
+          throw new Error(e.message);
+        });
+      return response;
     },
     staleTime: Infinity,
-  })
+  });
 
   const { data, isLoading, refetch, isFetching }: any = useQuery({
-    queryKey: ["pa", `${pagination.pageIndex * 10}_${filter !== "" ? "f" : ""}`],
+    queryKey: [
+      "pa",
+      `${pagination.pageIndex * 10}_${filter !== "" ? "f" : ""}`,
+    ],
     queryFn: async () => {
       const response: any = await request(
         "GET",
@@ -118,86 +124,152 @@ export default function DeliveryLists() {
       )
         .then((res: any) => res?.data?.value)
         .catch((e: Error) => {
-          throw new Error(e.message)
-        })
-      return response
+          throw new Error(e.message);
+        });
+      return response;
     },
     staleTime: Infinity,
     retry: 1,
-  })
+  });
 
   const handlerRefresh = React.useCallback(() => {
-    setFilter("")
-    setSortBy("")
+    setFilter("");
+    setSortBy("");
     setPagination({
       pageIndex: 0,
       pageSize: 10,
-    })
+    });
     setTimeout(() => {
-      Count.refetch()
-      refetch()
-    }, 500)
-  }, [])
+      Count.refetch();
+      refetch();
+    }, 500);
+  }, []);
 
   const handlerSortby = (value: any) => {
-    setSortBy(value)
+    setSortBy(value);
     setPagination({
       pageIndex: 0,
       pageSize: 10,
-    })
+    });
 
     setTimeout(() => {
-      refetch()
-    }, 500)
-  }
+      refetch();
+    }, 500);
+  };
 
   const handlerSearch = (value: string) => {
-    const qurey = value.replace("CardCode", "BPCode").replace("CardName", "BPName")
-    setFilter(qurey)
+    const qurey = value;
+    // .replace("CardCode", "BPCode")
+    // .replace("CardName", "BPName");
+    setFilter(qurey);
     setPagination({
       pageIndex: 0,
       pageSize: 10,
-    })
+    });
 
     setTimeout(() => {
-      Count.refetch()
-      refetch()
-    }, 500)
-  }
+      Count.refetch();
+      refetch();
+    }, 500);
+  };
+
+  const handlerSearchFilter = (queries: any) => {
+    if (queries === "") return;
+    handlerSearch("&$filter=" + queries);
+  };
 
   const handleAdaptFilter = () => {
-    setOpen(true)
-  }
+    setOpen(true);
+  };
+  const [searchValues, setSearchValues] = React.useState({
+    docnum: "",
+    cardcode: "",
+    cardname: "",
+  });
 
+  const handleGoClick = () => {
+    // Construct the query filters based on search values
+    let queryFilters = "";
+    if (searchValues.docnum) {
+      queryFilters += `DocNum eq ${searchValues.docnum}`;
+    }
+    if (searchValues.cardcode) {
+      queryFilters += queryFilters
+        ? ` and startswith(CardCode, '${searchValues.cardcode}')`
+        : `startswith(CardCode, '${searchValues.cardcode}')`;
+    }
+    if (searchValues.cardname) {
+      queryFilters += queryFilters
+        ? ` and startswith(CardName, '${searchValues.cardname}')`
+        : `startswith(CardName, '${searchValues.cardname}')`;
+    }
+
+    handlerSearchFilter(queryFilters);
+  };
   return (
     <>
-      <ModalAdaptFilter isOpen={open} handleClose={() => setOpen(false)}></ModalAdaptFilter>
-      <div className="w-full h-full px-6 py-2 flex flex-col gap-1 relative">
+      <ModalAdaptFilter
+        isOpen={open}
+        handleClose={() => setOpen(false)}
+      ></ModalAdaptFilter>
+      <div className="w-full h-full px-6 py-2 flex flex-col gap-1 relative bg-white">
         <div className="flex pr-2  rounded-lg justify-between items-center z-10 top-0 w-full  py-2">
-          <h3 className="text-base 2xl:text-base xl:text-base ">Sale / Delivery</h3>
-        </div>  
-        <div className="grid grid-cols-5 gap-3 mb-5 mt-4">
-          <MUITextField
+          <h3 className="text-base 2xl:text-base xl:text-base mx-2">
+            Sale / Delivery
+          </h3>
+        </div>
+        <div className="grid grid-cols-5 gap-3 mb-5 mt-4 mx-2">
+          {/* <MUITextField
             label="Search"
             placeholder="Search"
             className="bg-white"
             autoComplete="off"
-          />
+          /> */}
           <MUITextField
             label="Document No."
             placeholder="Document No."
             className="bg-white"
             autoComplete="off"
+            value={searchValues.docnum}
+            onChange={(e) =>
+              setSearchValues({ ...searchValues, docnum: e.target.value })
+            }
           />
-          <BPAutoComplete label="Customer" />
           <MUITextField
-            label="Posting Date"
-            placeholder="Posting Date"
+            label="Customer"
+            placeholder="Customer"
+            className="bg-white"
+            autoComplete="off"
+            value={searchValues.cardcode}
+            onChange={(e) =>
+              setSearchValues({ ...searchValues, cardcode: e.target.value })
+            }
+          />
+          <MUITextField
+            label="Customer Name"
+            placeholder="Customer "
+            className="bg-white"
+            autoComplete="off"
+            value={searchValues.cardname}
+            onChange={(e) =>
+              setSearchValues({ ...searchValues, cardname: e.target.value })
+            }
+          />
+          {/* <BPAutoComplete
+            label="Customer"
+            value={searchValues.cardcode}
+            onChange={(selectedValue) =>
+              setSearchValues({ ...searchValues, cardcode: selectedValue })
+            }
+          /> */}
+          <MUITextField
+            label="Delivery Date"
+            placeholder="Delivery Date"
             className="bg-white"
             type="date"
           />
           <div className="flex justify-end items-center align-center space-x-4 mt-4">
-            <Button variant="contained" size="small">
+            <Button variant="contained" size="small" onClick={handleGoClick}>
               Go
             </Button>
             <Button variant="outlined" size="small" onClick={handleAdaptFilter}>
@@ -205,6 +277,7 @@ export default function DeliveryLists() {
             </Button>
           </div>
         </div>
+        <div>
         <DataTable
           columns={columns}
           data={data}
@@ -217,7 +290,8 @@ export default function DeliveryLists() {
           paginationChange={setPagination}
           title="Delivery Lists"
         />
+        </div>
       </div>
     </>
-  )
+  );
 }
