64ac86286db76e4c8ad872712caf4fc9512c361b
Sopen <58549604+sopenlaz0@users.noreply.github.com>
2023-08-31 13:13:28 +0700

    fixing ui of login page

--- src/presentations/login/page/Login.tsx
+++ src/presentations/login/page/Login.tsx
@@ -1,80 +1,75 @@
-import React from "react"
-import TextField from "@mui/material/TextField"
-import LoadingButton from "@mui/lab/LoadingButton"
-import { useNavigate } from "react-router-dom"
-import { useCookies } from "react-cookie"
-import Alert from "@mui/material/Alert"
-import request from "../../../utilies/request"
-import AuthLogin from "../../../models/AuthLogin"
-import ItemGroupRepository from "../../../services/actions/itemGroupRepository"
-import UnitOfMeasurementRepository from "../../../services/actions/unitOfMeasurementRepository"
-import DepartmentRepository from "@/services/actions/departmentRepository"
-import PaymentMethodRepository from "../../../services/actions/paymentMethodRepository"
-import PaymentTermTypeRepository from "../../../services/actions/paymentTermTypeRepository"
-import OwnerRepository from "@/services/actions/ownerRepository"
-import ShippingTypeRepository from "../../../services/actions/shippingTypeRepository"
-import SalePersonRepository from "@/services/actions/salePersonRepository"
-import GLAccountRepository from "@/services/actions/GLAccountRepository"
-import VatGroupRepository from "@/services/actions/VatGroupRepository"
-import BranchRepository from "../../../services/actions/branchRepository"
-import WarehouseRepository from "@/services/warehouseRepository"
-import DistributionRuleRepository from "@/services/actions/distributionRulesRepository"
-import PriceListRepository from "@/services/actions/pricelistRepository"
-import UsersRepository from "@/services/actions/usersRepository"
-import CustomsGroupRepository from "@/services/actions/customsGroupRepository"
-import ManufacturerRepository from "@/services/actions/manufacturerRepository"
-import UnitOfMeasurementGroupRepository from "@/services/actions/unitOfMeasurementGroupRepository"
-import GetCurrentUserRepository from "../repository/getCurrencyUserRepository"
-import MUITextField from "@/components/input/MUITextField"
-
-// import sapLog from "../../../assets/sap_logo.png"
-import BizLogo from "../../../assets/img/big-logo.png"
-import { Collapse } from "@mui/material"
-import { BASE_BG_COLOR } from "@/configs"
-import ChartOfAccountsRepository from "@/services/actions/ChartOfAccountsRepository"
-import ProjectRepository from "@/services/actions/projectRepository"
+import React from "react";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import request from "../../../utilies/request";
+import AuthLogin from "../../../models/AuthLogin";
+import ItemGroupRepository from "../../../services/actions/itemGroupRepository";
+import UnitOfMeasurementRepository from "../../../services/actions/unitOfMeasurementRepository";
+import DepartmentRepository from "@/services/actions/departmentRepository";
+import PaymentMethodRepository from "../../../services/actions/paymentMethodRepository";
+import PaymentTermTypeRepository from "../../../services/actions/paymentTermTypeRepository";
+import OwnerRepository from "@/services/actions/ownerRepository";
+import ShippingTypeRepository from "../../../services/actions/shippingTypeRepository";
+import SalePersonRepository from "@/services/actions/salePersonRepository";
+import GLAccountRepository from "@/services/actions/GLAccountRepository";
+import VatGroupRepository from "@/services/actions/VatGroupRepository";
+import BranchRepository from "../../../services/actions/branchRepository";
+import WarehouseRepository from "@/services/warehouseRepository";
+import DistributionRuleRepository from "@/services/actions/distributionRulesRepository";
+import PriceListRepository from "@/services/actions/pricelistRepository";
+import CustomsGroupRepository from "@/services/actions/customsGroupRepository";
+import ManufacturerRepository from "@/services/actions/manufacturerRepository";
+import UnitOfMeasurementGroupRepository from "@/services/actions/unitOfMeasurementGroupRepository";
+import GetCurrentUserRepository from "../repository/getCurrencyUserRepository";
+import MUITextField from "@/components/input/MUITextField";
+import TelaCover from "../../../assets/img/tela-cover.jpeg";
+import BizLogo from "../../../assets/img/big-logo.png";
+import { BASE_BG_COLOR } from "@/configs";
+import ChartOfAccountsRepository from "@/services/actions/ChartOfAccountsRepository";
+import ProjectRepository from "@/services/actions/projectRepository";
 
 export default function Login() {
-  const [cookies, setCookie, removeCookie] = useCookies(["sessionId", "user"])
-  const [loading, setLoading] = React.useState(false)
-  const [message, setMessage] = React.useState("")
-  const navigate = useNavigate()
+  const [cookies, setCookie, removeCookie] = useCookies(["sessionId", "user"]);
+  const [loading, setLoading] = React.useState(false);
+  const [message, setMessage] = React.useState("");
+  const navigate = useNavigate();
 
-  const company = React.useRef("TMCT")
-  const username = React.useRef("manager")
-  const password = React.useRef("Admin@123")
+  const company = React.useRef("TMCT");
+  const username = React.useRef("manager");
+  const password = React.useRef("Admin@123");
 
   const onSubmit = async () => {
     try {
-      setLoading(true)
+      setLoading(true);
       // const auth = new AuthLogin(company.current, username.current, password.current);
-      const auth = new AuthLogin("SBODemoAU", "manager", "manager")
-      const response: any = await request("POST", "/Login", auth.toJson())
-      setCookie("sessionId", response?.data?.SessionId, { maxAge: 2000 })
-      const user = await GetCurrentUserRepository.post()
-      setCookie("user", user, { maxAge: 2000 })
-      await fetchAllDate()
-      navigate("/")
+      const auth = new AuthLogin("SBODemoAU", "manager", "manager");
+      const response: any = await request("POST", "/Login", auth.toJson());
+      setCookie("sessionId", response?.data?.SessionId, { maxAge: 2000 });
+      const user = await GetCurrentUserRepository.post();
+      setCookie("user", user, { maxAge: 2000 });
+      await fetchAllDate();
+      navigate("/");
     } catch (e: any) {
-      console.log(e)
-      setMessage(e?.message)
+      console.log(e);
+      setMessage(e?.message);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
 
   const onChange = (event: any, field: string) => {
     switch (field) {
       case "company":
-        company.current = event.target.value
-        break
+        company.current = event.target.value;
+        break;
       case "password":
-        password.current = event.target.value
-        break
+        password.current = event.target.value;
+        break;
       default:
-        username.current = event.target.value
+        username.current = event.target.value;
     }
-  }
+  };
 
   async function fetchAllDate(): Promise<void> {
     Promise.all([
@@ -101,71 +96,85 @@ export default function Login() {
       // vanna new modified
       await new ChartOfAccountsRepository().get(),
       await new ProjectRepository().get(),
-    ])
+    ]);
   }
 
   return (
-    <div className="w-full h-full flex justify-center items-center dark-theme">
-      <div className="w-[30rem] h-[60vh] flex flex-col justify-between gap-2 p-10 pb-0 rounded-xl shadow-xl border bg-white">
-        <div className="grow">
-          <div className="flex justify-between items-center">
-            <h2 className="font-bold text-xl ">TELA Station</h2>
-
-            <img src={BizLogo} className="w-[12rem]" />
-          </div>
-
-          <div className="h-10"></div>
-          <h2 className="font-bold text-2xl my-3">SIGN ME IN</h2>
-          <Collapse in={message !== ""}>
-            <div className="my-2">
-              {message && (
-                <Alert severity="error" onClose={() => setMessage("")}>
-                  {message}
-                </Alert>
-              )}
-            </div>
-          </Collapse>
-
-          <div className="flex flex-col gap-2">
-            <MUITextField
-              label="Company"
-              id="outlined-size-small"
-              size="small"
-              defaultValue={company.current}
-              onChange={(event) => onChange(event, "company")}
-            />
-            <MUITextField
-              label="Username"
-              id="outlined-size-small"
-              size="small"
-              defaultValue={username.current}
-              onChange={(event) => onChange(event, "username")}
-            />
-            <MUITextField
-              label="Password"
-              type="password"
-              id="outlined-size-small"
-              size="small"
-              defaultValue={password.current}
-              onChange={(event) => onChange(event, "password")}
+    <>
+      <div className="flex flex-col items-center justify-center h-[90vh] bg-gray-100">
+        <div className="w-full">
+          <div className="ml-24 mt-6 mb-4 sticky">
+            <img
+              src={BizLogo}
+              className="w-[18%] h-[18%]"
+              alt="Business Logo"
             />
           </div>
         </div>
 
-        <div className="h-20 w-full">
-          <LoadingButton
-            fullWidth
-            sx={{ backgroundColor: `${BASE_BG_COLOR} !important` }}
-            className="text-white"
-            variant="contained"
-            disableElevation
-            loading={loading}
-            onClick={onSubmit}
-          >
-            <span className="font-bold">Login</span>
-          </LoadingButton>
+        <div className="flex items-center justify-center w-full mt-4">
+          <div className="w-4/6 h-[60vh] bg-white border border-gray-300 shadow-xl flex rounded-xl p-2">
+            <div
+              className="w-8/12 lg:w-6/12 md:hidden sm:hidden bg-cover p-4 rounded-xl"
+            >
+              <img
+                className="rounded-l-lg rounded-r-sm"
+                src={TelaCover}
+                style={{
+                  display: "block", 
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                }}
+                alt="Background"
+              />
+            </div>
+            <div className="w-4/12 lg:w-6/12 md:w-full sm:w-full p-6 flex flex-col justify-center ">
+              <div className="flex flex-col space-y-4">
+                <h1 className="text-xl font-semibold">Sign In</h1>
+                <MUITextField
+                  label="Company"
+                  id="outlined-size-small"
+                  size="small"
+                  defaultValue={company.current}
+                  onChange={(event) => onChange(event, "company")}
+                />
+                <MUITextField
+                  label="Username"
+                  id="outlined-size-small"
+                  size="small"
+                  defaultValue={username.current}
+                  onChange={(event) => onChange(event, "username")}
+                />
+                <MUITextField
+                  label="Password"
+                  type="password"
+                  id="outlined-size-small"
+                  size="small"
+                  defaultValue={password.current}
+                  onChange={(event) => onChange(event, "password")}
+                />
+                <div className="pt-4 w-full">
+                  <LoadingButton
+                    fullWidth
+                    sx={{ backgroundColor: `${BASE_BG_COLOR} !important` }}
+                    className="text-white"
+                    variant="contained"
+                    loading={loading}
+                    onClick={onSubmit}
+                  >
+                    Login
+                  </LoadingButton>
+                </div>
+                <div className="mt-2 text-xs text-center text-[#656565]">
+                  <p> If you forgot your Username, please call</p>
+                  <p className="font-medium "> +855 (0) 23 925 333</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    </>
+  );
 }
