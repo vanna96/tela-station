177de12452458bb89cb1cd54da91d0346b78fcbe
vanna96 <sovannapoung@gmail.com>
2023-09-14 17:07:55 +0700

    Sopen (#9)
    * fixing ui of login page
    
    * add and fixing ui on sale order - added branch on general
    
    * add warehouse select by branch
    
    * .
    
    * fixing ui sale orders
    
    * continue fixing
    
    * filter itemcode , filter series
    
    * fix theme
    
    * fixing
    
    * finish general page
    
    * fixing
    
    * change font
    
    * fixing ui general and logistics
    
    * finish all ui changes
    
    * fixing filter listing sale order
    
    * add api for create edit and change details
    
    * fixing create sale order
    
    * finalizing sale orders
    
    * fix business line
    
    * fixing login page merge error
    
    * fixing
    
    ---------
    
    Co-authored-by: Sopen <58549604+sopenlaz0@users.noreply.github.com>

--- src/presentations/sale/sale_order/components/ItemModal.tsx
+++ src/presentations/sale/sale_order/components/ItemModal.tsx
@@ -7,7 +7,10 @@ import UOMTextField from "@/components/input/UOMTextField";
 import { getUOMGroupByCode } from "@/helpers";
 import WarehouseSelect from "@/components/selectbox/Warehouse";
 import DistributionRuleText from "@/components/selectbox/DistributionRuleTextField";
+<<<<<<< HEAD
+=======
 
+>>>>>>> 5cc08076ca76bab547573aed2a4404403417b657
 
 interface ItemModalProps {
   ref?: React.RefObject<ItemModal | undefined>;
@@ -163,8 +166,11 @@ export class ItemModal extends React.Component<ItemModalProps, any> {
                 onChange={(event) => this.handChange(event, "WarehouseCode")}
                 label="Business Line"
               />
-              <DistributionRuleText label="Product Line"  />
-              <DistributionRuleText label="Revenue Line"  />
+              <DistributionRuleText
+                label="Product Line"
+                onChange={(event) => this.handChange(event, "WarehouseCode")}
+              />
+              <DistributionRuleText label="Revenue Line" />
             </div>
           </div>
         </>
