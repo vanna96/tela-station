5d7ec07b76ced32485f1586bf815ff6387d25477
Sopen <58549604+sopenlaz0@users.noreply.github.com>
2023-10-17 09:05:01 +0700

    test

--- src/presentations/login/page/Login.tsx
+++ src/presentations/login/page/Login.tsx
@@ -99,7 +99,6 @@ export default function Login() {
       await new DistributionRuleRepository().get,
       await new CustomsGroupRepository().get(),
       await new ManufacturerRepository().get(),
-// test 
       // vanna new modified
       await new BranchBPLRepository().get(),
       await new ProjectRepository().get(),
