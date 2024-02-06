e3d77e0f912a09ef7bed07df7ff20b2d85012fe4
sopen <o.sophin29@gmail.com>
2024-02-06 15:43:28 +0700

    removed setting in content form

--- src/presentations/sale_order/sale_order_lob/components/ContentComponents.tsx
+++ src/presentations/sale_order/sale_order_lob/components/ContentComponents.tsx
@@ -1,9 +1,7 @@
 import React, { useMemo } from "react";
 import MaterialReactTable from "material-react-table";
 import { Button, Checkbox, IconButton, TextField } from "@mui/material";
-import { AiOutlineSetting } from "react-icons/ai";
 import FormCard from "@/components/card/FormCard";
-import { TbSettings } from "react-icons/tb";
 import Modal from "@/components/modal/Modal";
 import { BiSearch } from "react-icons/bi";
 import MUITextField from "@/components/input/MUITextField";
@@ -141,10 +139,6 @@ export default function ContentComponent(props: ContentComponentProps) {
               Remove
             </span>
           </Button>
-
-          <IconButton onClick={() => columnRef.current?.onOpen()}>
-            <TbSettings className="text-2lg" />
-          </IconButton>
         </div>
       }
     >
@@ -200,9 +194,6 @@ export default function ContentComponent(props: ContentComponentProps) {
             muiTableBodyRowProps={() => ({
               sx: { cursor: "pointer" },
             })}
-            icons={{
-              ViewColumnIcon: (props: any) => <AiOutlineSetting {...props} />,
-            }}
             enableTableFooter={false}
           />
           <div className="grid grid-cols-12 mt-2">
@@ -315,143 +306,7 @@ export default function ContentComponent(props: ContentComponentProps) {
             </div>
           </div>
         </div>
-        <ContentTableSelectColumn
-          ref={columnRef}
-          columns={props.columns}
-          visibles={colVisibility}
-          onSave={(value) => {
-            setColVisibility(value);
-          }}
-        />
       </>
     </FormCard>
   );
-}
-
-interface ContentTableSelectColumnProps {
-  ref?: React.RefObject<ContentTableSelectColumn | undefined>;
-  onSave?: (value: any) => void;
-  columns: any[];
-  visibles: any;
-}
-
-class ContentTableSelectColumn extends React.Component<
-  ContentTableSelectColumnProps,
-  any
-> {
-  constructor(props: any) {
-    super(props);
-
-    this.state = {
-      open: false,
-      searchColumn: "",
-      showChecks: false,
-      visibles: {},
-    } as any;
-
-    this.onOpen = this.onOpen.bind(this);
-    this.onClose = this.onClose.bind(this);
-    this.onSave = this.onSave.bind(this);
-    this.handChange = this.handChange.bind(this);
-    this.handlerChangeColVisibility =
-      this.handlerChangeColVisibility.bind(this);
-  }
-
-  componentDidMount(): void {}
-
-  onOpen(data?: any) {
-    this.setState({ open: true, visibles: { ...this.props.visibles } });
-  }
-
-  onClose() {
-    this.setState({ open: false });
-  }
-
-  onSave() {
-    if (this.props.onSave) {
-      this.props.onSave(this.state.visibles);
-    }
-
-    this.setState({ open: false });
-  }
-
-  handChange(event: any) {
-    this.setState({ ...this.state, searchColumn: event.target.value });
-  }
-
-  handlerChangeColVisibility(event: any, field: string) {
-    const visibles = { ...this.state.visibles };
-    visibles[field] = event.target.checked;
-    this.setState({
-      ...this.state,
-      visibles: { ...this.props.visibles, ...visibles },
-    });
-  }
-
-  render() {
-    return (
-      <Modal
-        title={`Columns Setting`}
-        titleClass="pt-3 px-2 font-bold w-full"
-        open={this.state.open}
-        widthClass="w-[40rem]"
-        heightClass="h-[80vh]"
-        onClose={this.onClose}
-        onOk={this.onSave}
-        okLabel="Save"
-      >
-        <div className="w-full h-full flex flex-col ">
-          <div className="flex justify-between sticky top-0 bg-white py-2 z-10 border-b">
-            <div className="flex">
-              <div>
-                {" "}
-                <Checkbox
-                  size="small"
-                  className="mt-2"
-                  defaultChecked={this.state.showChecks}
-                  onChange={(e) =>
-                    this.setState({
-                      ...this.state,
-                      showChecks: !this.state.showChecks,
-                    })
-                  }
-                />
-              </div>
-              <label htmlFor="showAll" className="flex items-center ">
-                Show Selected
-              </label>
-            </div>
-            <div className="flex w-[15rem] items-center">
-              <MUITextField
-                placeholder="Search Column..."
-                onChange={this.handChange}
-                endAdornment
-                endIcon={<BiSearch className="text-sm" />}
-              />
-            </div>
-          </div>
-          <ul className=" text-[14px] grid grid-cols-1 mt-3 ">
-            {this.props.columns
-              .filter((val) =>
-                val.header
-                  .toLowerCase()
-                  .includes(this.state.searchColumn.toLowerCase())
-              )
-              .map((e) => (
-                <li key={shortid.generate()} className={`border-b`}>
-                  <Checkbox
-                    checked={this.state.visibles[e?.accessorKey] ?? false}
-                    onChange={(event) =>
-                      this.handlerChangeColVisibility(event, e?.accessorKey)
-                    }
-                    size="small"
-                  />{" "}
-                  <span>{e?.header} </span>
-                </li>
-              ))}
-          </ul>
-        </div>
-      </Modal>
-    );
-  }
 }
